54da1920c22cfb1c0f41065213195c8c0c15b53a
Vanya Slesarev <119366827+shashlyk55@users.noreply.github.com>
2024-04-11 00:57:14 +0300

    Add files via upload

---
+++ JobSearchApp/JobSearchApp/model/Vacancy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearchApp.model
+{
+    public class Vacancy
+    {
+        private string _companyLogo;
+        private string _jobTitle;
+        private int _salary;
+        private DateTime _date;
+        public string CompanyLogo { get; set; }
+        public string JobTitle { get; set; }
+        public int Salary { get; set; }
+        public DateTime DateAdded { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        public Vacancy(string companyLogo, string jobTitle, int salary, DateTime dateAdded)
+        {
+            CompanyLogo = companyLogo;
+            JobTitle = jobTitle;
+            Salary = salary;
+            DateAdded = dateAdded;
+        }
+
+        public object Clone()
+        {
+            if (this == null)
+                return null;
+            return new Vacancy(CompanyLogo, JobTitle, Salary, DateAdded);
+        }
+
+    }
+}
---
+++ JobSearchApp/JobSearchApp/userControl/PlaceholderTextBoxControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JobSearchApp.userControl
+{
+    /// <summary>
+    /// Логика взаимодействия для PlaceholderTextBoxControl.xaml
+    /// </summary>
+    public partial class PlaceholderTextBoxControl : UserControl
+    {
+        public static readonly DependencyProperty PlaceholderTextProperty =
+            DependencyProperty.Register("PlaceholderText", typeof(string), typeof(PlaceholderTextBoxControl));
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(PlaceholderTextBoxControl));
+
+        public string PlaceholderText
+        {
+            get { return (string)GetValue(PlaceholderTextProperty); }
+            set { SetValue(PlaceholderTextProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public PlaceholderTextBoxControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
